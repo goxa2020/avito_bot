39fa339e628c26e03e0c679641105f2a40556687
Georgy <georgy11111122222@gmail.com>
2022-12-13 22:38:51 +0300

    Проверка

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ Bot_name = 'testtikk_bbot'
 chanel_name = '@ttttttttesst'
 chanel_id = '-1001658466096'
 no_photo = 'AgACAgIAAxkBAAIK0mK3oQuVT6G3K7LjOscDsJL5ouYWAAKEwTEb47e4ScWJE5z958-bAQADAgADcwADKQQ'
-# i love u
+# I love u very much
