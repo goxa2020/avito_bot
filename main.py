3145a56768ea7c5c5b117076aa74266b993b027f
georgy <georgy111111222222@gmail.com>
2023-03-24 01:27:09 +0300

    Настройка Pycharm

--- main.py
+++ main.py
@@ -36,3 +36,4 @@ async def on_shutdown(_):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=start_on, on_shutdown=on_shutdown)  # Запускаем бота
+    # люблю
