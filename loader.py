8fb27983abf73e4945e03b76c920d2a67beb14f6
georgy <georgy111111222222@gmail.com>
2023-04-06 03:03:02 +0300

    ignore update

--- loader.py
+++ loader.py
@@ -8,6 +8,6 @@ import config
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
 
 storage = MemoryStorage()
-db = Sqlighter("database.db")
+db = Sqlighter("ignore/database.db")
 bot = Bot(token=config.Bot_token)
 dp = Dispatcher(bot, storage=storage)
